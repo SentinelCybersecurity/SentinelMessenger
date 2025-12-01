12377eaac8705fde5e2e7e08e01bb356174ff687
Andrea Bruno <andrea_bruno@hotmail.com>
2025-12-02 00:29:23 +0100

    Regular Update 02/12/2025

--- update.bat
+++ update.bat
@@ -1,11 +1,5 @@
-﻿@echo off
-echo Adding changes to Git...
-git add .
-
-echo Committing with message: "Regular Update"
-git commit -m "Regular Update"
-
-echo Pushing to origin main...
-git push origin main
-
-echo Update completed.
+@echo off
+git fetch origin && git merge -X ours origin/main
+git add . && git commit -m "Regular Update %DATE%"
+git push -f origin main
+echo Done.
